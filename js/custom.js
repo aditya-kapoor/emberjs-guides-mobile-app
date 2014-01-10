d545fa31a6c15a473a3df30493f3866086dcdd7c
aditya-kapoor <aditya.kapoor@vinsol.com>
2014-01-10 15:14:31 +0530

    JS Objectification in progress

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,44 @@
-$(document).bind('pageinit', function(){
-  $('.guide-links').click(function(event){
-    $parent_el  = $(this).closest('ul')
-    parent_dir  = $parent_el.data("source-directory").toString()
-    source_file = $(this).data("source-file").toString()
-    $that       = $(this)
-    $.ajax({
-      method: "GET",
-      url: parent_dir+source_file+".md",
-      success: function(data, status, xhr){
-        convertor = new Showdown.converter()
-        html = convertor.makeHtml(data.toString())
-        $('#content-page #page-title').html($that.text())
-        $('#content-page #page-body').html(html)
-        $.mobile.navigate("#content-page")
+EmberApp = {
+  convertor: {},
+  bindEvents: function(){
+    $('a').click(function(event){
+      if($(this).hasClass("guide-links")){
+        EmberApp.handleAJAXforGuideLinks($(this))
+        event.preventDefault();
+      }else{
+        if($(this).attr("href").indexOf("http://") != 0){
+          console.log($(this))
+        }
       }
     })
-    event.preventDefault();
-  })
+  },
+  handleAJAXforGuideLinks: function(element){
+    $parent_el  = $(element).closest('ul')
+    parent_dir  = $parent_el.data("source-directory").toString()
+    source_file = $(element).data("source-file").toString()
+    EmberApp.sendAJAX(parent_dir+source_file+".md", element)
+  },
+  sendAJAX: function(url, element){
+    $.ajax({
+      method: "GET",
+      url: url,
+      success: function(data, status, xhr){
+        page_html = EmberApp.makeHtml(data)
+        EmberApp.navigateToPage(page_html, $(element).text())
+      }
+    })
+  },
+  makeHtml: function(data){
+    EmberApp.convertor = new Showdown.converter()
+    return EmberApp.convertor.makeHtml(data.toString())
+  },
+  navigateToPage: function(html, title){
+    $('#content-page #page-title').html(title)
+    $('#content-page #page-body').html(html)
+    $.mobile.navigate("#content-page")
+  }
+}
+
+$(document).bind('pageinit', function(){
+  EmberApp.bindEvents()
 })
